9e1415195eeab34e6d794cf1674d32a5ef19a746
Camille Reynders <camille.reynders@gmail.com>
2015-04-03 09:22:19 +0200

    Expound help on command line

--- cli.js
+++ cli.js
@@ -5,11 +5,11 @@ var headstone = require('./');
 
 var cli = meow({
   help: [
-    'Usage',
-    '  headstone <input>',
+    'Usage:',
+    '  headstone <file> [--cwd=<path>] [--models=<path>] [--configFile=<file>] [--mongoUri=<uri>]',
     '',
     'Example',
-    '  headstone disableUsers'
+    '  headstone scripts/disableUsers --models=./app/models --mongoUri=mongodb://localhost/myDb'
   ].join('\n')
 });
 headstone(cli.input, cli.flags);
